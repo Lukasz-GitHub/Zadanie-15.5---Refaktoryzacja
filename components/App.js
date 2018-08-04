85c20fdaa9a8926c5ca505216237bd7d258652de
Lukasz Borek <borek_lukasz@outlook.com>
2018-08-04 16:05:50 +0200

    Initial commit

---
+++ components/App.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var GIPHY_API_URL = 'http://api.giphy.com';
+var GIPHY_PUB_KEY = 'PE1pvqflpzm6OIu6vxHFpPK6e14Ulsap';
+
+App = React.createClass({
+    
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+    
+    handleSearch: function(searchingText) { // Pobierz na wejściu wpisywany tekst.
+        this.setState({
+            loading: true // Zasygnalizuj, że zaczął się proces ładowania.
+        });
+        this.getGif(searchingText).then(gif => { // Rozpocznij pobieranie gifa.
+            this.setState({ // Na zakończenie pobierania:
+                loading: false, // Przestań sygnalizować ładowanie.
+                gif: gif, // Ustaw nowego gifa z wyniku pobierania.
+                searchingText: searchingText // Ustaw nowy stan dla wyszukiwanego tekstu.
+            });
+        })
+        .catch(error => console.error('Error!', error));
+    },
+    
+    getGif: function(searchingText) {
+        return new Promise(
+            function (resolve, reject) {
+                var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        var data = JSON.parse(xhr.responseText).data;
+                            var gif = {
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                            };
+                        resolve(gif);
+                    } else {
+                        reject(new Error(this.statusText));
+                    }
+                };
+                xhr.onerror = function () {
+                  reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+                };
+                xhr.send();
+            });
+    },
+    
+    render: function() {
+        
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+        
+        return (
+            <div style={styles}>
+                <h1 style={{borderBottom: '1px solid black'}}>Wyszukiwarka GIFów</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com' target='_blank' style={{textDecoration: 'none', color: 'blue'}}>GIPHY.COM</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search onSearch={this.handleSearch} />
+                <Gif
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />
+            </div>
+        );
+    }
+});
